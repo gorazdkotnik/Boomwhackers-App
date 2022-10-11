58108937aa2fced1657583a0aeaa355b37e0e424
gorazdkotnik <gorazdkotnik05@gmail.com>
2022-10-11 09:15:40 +0200

    adde base for form 2

---
+++ Boomwhackers/Form2.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Boomwhackers
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.formTitleLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.projectNameLabel = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // formTitleLabel
+            // 
+            this.formTitleLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.formTitleLabel.Location = new System.Drawing.Point(0, 0);
+            this.formTitleLabel.Name = "formTitleLabel";
+            this.formTitleLabel.Size = new System.Drawing.Size(800, 23);
+            this.formTitleLabel.TabIndex = 0;
+            this.formTitleLabel.Text = "Ustvari nov projekt";
+            this.formTitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(349, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // projectNameLabel
+            // 
+            this.projectNameLabel.AutoSize = true;
+            this.projectNameLabel.Location = new System.Drawing.Point(13, 68);
+            this.projectNameLabel.Name = "projectNameLabel";
+            this.projectNameLabel.Size = new System.Drawing.Size(81, 16);
+            this.projectNameLabel.TabIndex = 2;
+            this.projectNameLabel.Text = "Ime projekta";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.projectNameLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.formTitleLabel);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label formTitleLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label projectNameLabel;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+    }
+}
