3f784aadee747a733df3531bea254a383bc82abb
AnzeBlaBla <anzemajblabla@gmail.com>
2023-05-16 09:33:09 +0200

    Popravljeni soundi (vsi)

--- Boomwhackers/Audio/CachedSound.cs
+++ Boomwhackers/Audio/CachedSound.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
 using NAudio.Wave;
 
 namespace Boomwhackers.Audio
@@ -11,34 +13,28 @@ namespace Boomwhackers.Audio
     {
         public float[] AudioData { get; private set; }
         public WaveFormat WaveFormat { get; private set; }
+
+        public void WriteResourceToFile(string resourceName, string fileName)
+        {
+            var resource = Properties.Resources.ResourceManager.GetObject(resourceName);
+            // cast to memory stream
+            var stream = resource as MemoryStream;
+            using (var file = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            {
+                stream.CopyTo(file);
+            }
+        }
         public CachedSound(string audioFileName)
         {
-            /*if (audioFileName.StartsWith("resources://"))
+            if (audioFileName.StartsWith("resources://"))
             {
+                // Make a file out of the sound
+                string fileName = Path.GetTempFileName();
+                // extract resource name
                 string resourceName = audioFileName.Substring(12);
-
-                MemoryStream ms = new MemoryStream(StreamToByteArray(Properties.Resources.ResourceManager.GetStream(resourceName)));
-
-                using (var waveFileReader = new WaveFileReader(ms))
-                {
-                    // Convert to float[] (AudioData)
-
-                    WaveFormat = waveFileReader.WaveFormat;
-                    var wholeFile = new List<float>((int)(waveFileReader.Length / 4));
-                    byte[] readBuffer = new byte[waveFileReader.WaveFormat.SampleRate * waveFileReader.WaveFormat.Channels * 4];
-                    int bytesRead;
-                    while ((bytesRead = waveFileReader.Read(readBuffer, 0, readBuffer.Length)) > 0)
-                    {
-                        for (int n = 0; n < bytesRead; n += 4)
-                        {
-                            wholeFile.Add(BitConverter.ToSingle(readBuffer, n));
-                        }
-                    }
-                    AudioData = wholeFile.ToArray();
-                }
+                WriteResourceToFile(resourceName, fileName);
+                audioFileName = fileName;
             }
-            else
-            {*/
             using (var audioFileReader = new AudioFileReader(audioFileName))
             {
                 WaveFormat = audioFileReader.WaveFormat;
@@ -51,7 +47,6 @@ namespace Boomwhackers.Audio
                 }
                 AudioData = wholeFile.ToArray();
             }
-            /*}*/
 
         }
 
